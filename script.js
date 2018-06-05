eed51ab2c4e0d05d9f762c0fc8c60f14badaaa7d
ewarzech <ewarzech@gmail.com>
2018-06-05 19:09:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,10 +17,50 @@ function initMap() {
     map: map,
     title: 'Park Jordana od ul. Reymonta'
   });
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '<div id="bodyContent">'+
+      '<p>nazwa: Park Jordana</p>'+
+      '<p>lokalizacja: Al. 3 Maja</p>'+
+      '<p>godziny otwarcia: 10:00 - 18:00</p>'+
+      '</div>';
+  
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(50.064708, 19.916431),
+    map: map,
+    title: 'Park Jordana od ul. Reymonta'
+  });
+  
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng(50.062143, 19.937548),
     map: map,
     title: 'Rynek Główny'
+  });
+    var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '<div id="bodyContent">'+
+      '<p>nazwa: Rynek Główny</p>'+
+      '<p>lokalizacja: Rynek Główny 1</p>'+
+      '<p>godziny otwarcia: 7:00 - 22:30</p>'+
+      '</div>';
+  
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(50.062143, 19.937548),
+    map: map,
+    title: 'Rynek Główny'
+  });
+  
+  
+  
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
   });
   
 }
